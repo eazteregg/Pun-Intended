baa83b0d10f83eecd3c5c49a9fe8704e1e2d0d0b
Jon Dehdari <jon@dehdari.org>
2017-05-26 13:21:56 +0200

    main.py: added dummy cmd args

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
+import argparse
 from SearchEngine import SearchEngine
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Helps you build puns and lyrics'
+        parser.add_argument('--vecs', type=str, default="glove.6B.100d.txt", help="Path to Glove/word2vec file (default: %(default)s)"
+        parser.add_argument('--rhyme', action="store_true", help="Restrict phonological matches to rhymes"
+        parser.add_argument('--ortho', action="store_true", help="Use orthographic matches instead of phonological ones"
+        )
+    cmd_args = parser.parse_args()
 
     print("Hello and welcome to the pun aid!")
     se = SearchEngine(1000)
