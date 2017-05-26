9df8459be92040c51d37baf1a640151b1d190b23
Jon Dehdari <jon@dehdari.org>
2017-05-26 13:25:16 +0200

    main.py: fix cmd args

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from SearchEngine import SearchEngine
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Helps you build puns and lyrics'
-        parser.add_argument('--vecs', type=str, default="glove.6B.100d.txt", help="Path to Glove/word2vec file (default: %(default)s)"
-        parser.add_argument('--rhyme', action="store_true", help="Restrict phonological matches to rhymes"
-        parser.add_argument('--ortho', action="store_true", help="Use orthographic matches instead of phonological ones"
-        )
+        description='Helps you build puns and lyrics')
+    parser.add_argument('--vecs', type=str, default="glove.6B.100d.txt", help="Path to Glove/word2vec file (default: %(default)s)")
+    parser.add_argument('--rhyme', action="store_true", help="Restrict phonological matches to rhymes")
+    parser.add_argument('--ortho', action="store_true", help="Use orthographic matches instead of phonological ones")
     cmd_args = parser.parse_args()
 
     print("Hello and welcome to the pun aid!")
