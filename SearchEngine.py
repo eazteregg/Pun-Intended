08530a940c6fd16915c5043edbe3372fbf29d7a2
eazteregg <jere@packhaus.info>
2017-05-26 15:20:39 +0200

    Added list combination possibilities and SearchEngine now also accepts gloVe vector files and binaries.

--- SearchEngine.py
+++ SearchEngine.py
@@ -1,4 +1,4 @@
-import gensim
+import gensim.scripts.glove2word2vec
 from levenshtein import levenshtein
 from gensim.models.keyedvectors import KeyedVectors as kv
 import sys
@@ -8,18 +8,51 @@ import os
 class SearchEngine():
     """This is the class that will handle all of the operations and queries and such"""
 
-    def __init__(self, d_of_comparisons, n_of_results=5, combine='s'):
+    def __init__(self, d_of_comparisons, vectorfile, n_of_results=5, combine='s'):
 
         self.d_of_comparisons = d_of_comparisons  # max dimensionality of the two lists
-        self.n_of_results = n_of_results  # how many resutlts the search engine is supposed to outpu
+        self.n_of_results = n_of_results  # how many resutlts the search engine is supposed to output
         self.combine = combine  # later to be implemented as choice between combination operations
-        self.word_vectors = kv.load_word2vec_format(os.path.join('data', 'glove.6B.100d.txt'))  # retrieve word vectors from file
+
+        #If vector file in gloVe format, tranfsorm it into word2vec and provides option to store it as binary
+
+        create_bin = 'n'
+
+        try:
+            if sys.argv[1][-4:] == ".bin":
+                binary = True
+            else:
+                for filename in os.listdir('data'):
+                    if filename == 'word2vec.' + vectorfile[:-4] + '.bin':
+                        print("Found binary file with the same name as the specified one. Will be loading the binary.")
+                        vectorfile = filename
+                        binary = True
+                        break
+                    binary = False
+
+            self.word_vectors = kv.load_word2vec_format(os.path.join('data', vectorfile), binary=binary)  # retrieve word vectors from file
+
+            if not binary:
+                create_bin = input("Would you like to create a binary file for your vector file, so that future loading times may be shortened? y/n")
+
+        except ValueError as v:
+            print(v)
+            print("Converting gloVe to word2vec format.-------------")
+
+            create_bin = input("Would you like to create a binary file for your vector file, so that future loading times may be shortened? y/n")
+
+            gensim.scripts.glove2word2vec.glove2word2vec(os.path.join('data', vectorfile), os.path.join('data', 'word2vec.' + vectorfile))
+            self.word_vectors = kv.load_word2vec_format(os.path.join('data', 'word2vec.' + vectorfile))
+
+        if create_bin == 'y':
+            self.word_vectors.save_word2vec_format(os.path.join('data', 'word2vec.' + vectorfile[:-4] + '.bin'), binary=True)
+
         self.phondict = dict()
 
         # Fill the phonetic dictionary
         with open(os.path.join('data', 'cmudict-0.7b.utf8')) as phondict:
             for line in phondict:
-                if line[:3] == ";;;":
+                if line[:3] == ";;;":   #skip the first couple of lines
                     continue
 
                 line = line.split(maxsplit=1)
@@ -44,7 +77,7 @@ class SearchEngine():
         results.sort(key=lambda x: x[1])
         return [x[0] for x in results[:self.d_of_comparisons]]
 
-    def combines(self, ass_list, phonlist, fn_combo=lambda x, y: x + y):
+    def combines(self, ass_list, phonlist):
 
         """Combines the associative list with the phonetic list. To be implemented: fn_combo which steers the 
         combination operation"""
@@ -53,23 +86,40 @@ class SearchEngine():
 
         # raise ValueError("listA must have the same length as listB!") # ...required
 
-        resdict = dict()
-        for x in range(len(ass_list)):
-            if ass_list[x] in phonlist:
-                resdict[ass_list[x]] = x
-            else:
-                resdict[ass_list[x]] = 100000000 + x
-        for y in range(len(phonlist)):
-            try:
-                resdict[phonlist[y]] += y
-            except KeyError:
-                resdict[phonlist[y]] = 100000000 + y
+        fn_combo = None
+        if self.combine == 's':
+            fn_combo = lambda m, n: m+n
+
+        elif self.combine == 'p':
+            fn_combo = lambda m, n: (m+1)*(n+1) # If either number is 0, the calculation is senseless
+
+        if fn_combo:        #If either summation or multiplication has been chosen as combination method
+
+            resdict = dict()
+            for x in range(len(ass_list)):
+                if ass_list[x] in phonlist:
+                    resdict[ass_list[x]] = x
+                else:
+                    resdict[ass_list[x]] = 100000000 + x
+            for y in range(len(phonlist)):
+                try:
+                    resdict[phonlist[y]] = fn_combo(resdict[phonlist[y]], y)
+                except KeyError:
+                    resdict[phonlist[y]] = 100000000 + y
+
+        else:               #If nothing has been chosen -> default to intersection
+
+            return [x for x in ass_list if x in phonlist]
 
         reslist = []
+
         for word in resdict:
             reslist.append((word, resdict[word]))
+
         reslist.sort(key=lambda x: x[1])
+
         print([x for x in ass_list if x in phonlist])
+
         return reslist[:self.n_of_results]
         # return [x[0] for x in reslist[:self.n_of_results]]
 
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 from SearchEngine import SearchEngine
-
+import sys
 if __name__ == "__main__":
 
+    if len(sys.argv) < 2:
+        print("Please specify the word2vec or gloVe vector file as 2nd argument.")
+        sys.exit()
+
     print("Hello and welcome to the pun aid!")
-    se = SearchEngine(1000)
+    se = SearchEngine(1000, sys.argv[1], combine='s')
 
     while True:
 
@@ -14,8 +18,20 @@ if __name__ == "__main__":
 
         if not query:
             break
-        elif len(query) != 2:
+
+        elif len(query) > 2:
             print("Sorry, you need to provide two arguments!")
             continue
+        elif len(query) == 1:
+            if query[0] == '-s':
+                se.combine = 's'
+            elif query[0] == '-p':
+                se.combine = 'p'
+            elif query[0] == '-i':
+                se.combine = ''
+            else:
+                print('To change combination method please use: -i -p -s ')
+                continue
+        else:
 
-        print(se.execute_query(query[0], query[1]))
+            print(se.execute_query(query[0], query[1]))
