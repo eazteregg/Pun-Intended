0290f4dea3c07a47df5a50c407501483cd3b9df5
Maximilian <s9mnwol2@stud.uni-saarland.de>
2017-06-01 19:51:41 +0200

    reformat

--- generation/implement_baseline.py
+++ generation/implement_baseline.py
@@ -6,41 +6,41 @@ Created on Thu May 25 19:55:45 2017
 """
 import argparse
 from nltk.metrics.distance import edit_distance
-MAX_DISTANCE = 2 # Max edit distance to consider when exchanging words.
+
+MAX_DISTANCE = 2  # Max edit distance to consider when exchanging words.
 
 parser = argparse.ArgumentParser(description='Implement words in idioms')
 # Required word argument
 parser.add_argument('sounds_like', type=str,
                     help='sounds like x')
-		
 
 # Required word argument
 """
 parser.add_argument('meaning', type=str,
                     help='meaning associated with y')"""
-					
+
 # Optional edit distance argument
 parser.add_argument('dist_arg', type=int, nargs='?',
-                    help='An optional integer maximum edit distance argument')					
+                    help='An optional integer maximum edit distance argument')
 
 args = parser.parse_args()
 if args.dist_arg: MAX_DISTANCE = args.dist_arg
 
 # The complete corpus, where every idiom has its explanation in the next line.
-corpus = [] # 310
-corpus_idioms = [] # Idioms only (155)
-corpus_explanations = [] # Explanations only (155)
+corpus = []  # 310
+corpus_idioms = []  # Idioms only (155)
+corpus_explanations = []  # Explanations only (155)
 
 with open('data/idiom_corpus_small.txt', 'r') as corpus:
     corpus = [line.strip() for line in corpus if not line.startswith('#')]
-    
+
 corpus_idioms = corpus[0::2]
 corpus_explanations = corpus[1::2]
 
 # Get words from the user
 x = args.sounds_like
 
-#meaning = args.meaning
+# meaning = args.meaning
 
 # Implement word in sentence
 
@@ -51,6 +51,5 @@ for line in corpus_idioms:
         if distance != 0 and distance <= MAX_DISTANCE:
             sentence = line.replace(word, x)
             sentences.append(sentence)
-            
+
 print(sentences)
-        
